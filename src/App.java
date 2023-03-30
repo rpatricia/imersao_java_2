8cccaa059d91f8f1d27491499f4f713e962a1e33
Renata Pausini <rpatricia@outlook.com>
2023-03-29 21:13:19 -0300

    aula-2

--- src/App.java
+++ src/App.java
@@ -1,4 +1,6 @@
+import java.io.InputStream;
 import java.net.URI;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -26,14 +28,17 @@ public class App {
         List<Map<String, String>> listaDeFilmes = parser.parse(body);
 
 
-
+        GeradoraDeFigurinhas gerador = new GeradoraDeFigurinhas();
         // exibir e manipular os dados 
         for (Map<String,String> filme : listaDeFilmes) {
             System.out.println(filme.get("title"));
             System.out.println(filme.get("image"));
             System.out.println(filme.get("imDbRating"));
-            System.out.println("\u2B50");
+            System.out.println("\u2B50");            
             System.out.println();
+            String nome = filme.get("title").replace(":", "-") + ".png";
+            InputStream inputStream = new URL(filme.get("image")).openStream();
+            gerador.cria(inputStream, nome);
         }
     }
 }
